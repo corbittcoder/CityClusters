e2cebd723ef5eccab906ffe3ab0058fb02124079
Scott A. Corbitt <scott.a.corbitt@gmail.com>
2021-12-03 00:20:45 -0800

    Keep track of the total man-hours by group now

--- knn.py
+++ knn.py
@@ -4,8 +4,11 @@ import sys
 pd.options.mode.chained_assignment = None
 import plot
 
+MILES_PER_LAT = 69
+MILES_PER_LNG = 53
+MILES_PER_HOUR = 30
 def knn(places, numGroups):
-    numTimes = 20
+    numTimes = 5
     prevPlaces = pd.DataFrame()
     while not places.equals(prevPlaces) and numTimes > 0:
         prevPlaces = places.copy()
@@ -15,7 +18,7 @@ def knn(places, numGroups):
             # distances = np.zeros(numGroups)
             #Get 5 closest points to current point
             places['Distances'] = abs(places.Lng - row.Lng) + abs(places.Lat - row.Lat)
-            colors = places.sort_values(by=['Distances']).iloc[0:20]['Color'].unique()[0:3]
+            colors = places.sort_values(by=['Distances']).iloc[0:50]['Color'].unique()[0:3]
             distances = pd.Series(data = np.zeros(len(colors)), index = colors)
             mindex = colors[0] #index of minimum distance
             for i in colors:
@@ -26,14 +29,11 @@ def knn(places, numGroups):
                 #Calculate distance from all points
                 color.Lng -= row['Lng']
                 color.Lat -= row['Lat']
-                milesPerLat = 69
-                milesPerLng = 53
-                milesPerHour = 45
-                color['Distance'] = (abs(color.Lng) * milesPerLng + abs(color.Lat) * milesPerLat)
+                color['Distance'] = (abs(color.Lng) * MILES_PER_LNG + abs(color.Lat) * MILES_PER_LAT)
                     #color.assign(Distance=lambda color: (color.Lng ** 2 + color.Lat ** 2) ** .5)
 
                 # distances[i] = color.Distance.min(axis=0)
-                distances[i] = color.Distance.sum() / milesPerHour / len(color) * row.NumVisits + color.Hours.sum() #don't want the cells to get too big
+                distances[i] = (color.Distance.sum() / MILES_PER_HOUR / len(color) * row.NumVisits) + (color.Hours.sum() / 10) #don't want the cells to get too big
 
                 if row.Color == i:
                     distances[i] -= row.Hours
@@ -46,26 +46,41 @@ def knn(places, numGroups):
 
 def main(places, name, numGroups, api):
     #Group by zip code
-    hours = places.groupby(['ShortZip'])['Hours'].sum()
     zips = places.drop_duplicates(subset=['ShortZip'])
 
     #Sort by zip code groups to get rough groups
-    zips['Hours'] = zips.ShortZip.replace(hours)
+    # zips['Hours'] = zips.ShortZip.replace(hours)
+    zips['Hours'] = np.zeros(len(zips))
     zips['Color'] = np.random.randint(0, numGroups, zips.shape[0])
     zips['NumVisits'] = np.ones(len(zips))
     zips = knn(zips, numGroups)
 
     #Assign groups to full dataframe
     places['Color'] = places.ShortZip.replace(zips.set_index('ShortZip')['Color'])
+    plot.plot(places, name, api) #compare pre- to post-sorting
+    # #now measure with just distance
+    # distanceOnly = places.copy()
+    # distanceOnly['Hours'] = np.zeros(len(places))
+    # distanceOnly = knn(distanceOnly, numGroups)
     #Now get exact groups starting from rough basis
     places = knn(places, numGroups)
+
+    #Calculate man-hours for each group
+    hours = places.groupby(['Color'])['Hours'].sum()
+    for color, totalHours in hours.items():
+        group = places[places.Color == color]
+        group.Lat -= group['Lat'].mean()
+        group.Lng -= group['Lng'].mean()
+        group['Distance'] = (abs(group.Lng) * MILES_PER_LNG + abs(group.Lat) * MILES_PER_LAT) * group.NumVisits
+        hours[color] = group.Distance.sum() / MILES_PER_HOUR + totalHours
 
     #plot data points on map
     try:
         places.to_csv('files\\' + name + '_results.csv', index = False)
+        hours.to_csv('files\\' + name + '_hours.csv', index=False)
     except:
         print("Couldn't save")
-    plot.plot(places, name, numGroups, api)
+    plot.plot(places, name, api)
 
 if __name__ == '__main__':
     api = ''
--- plot_group.py
+++ plot_group.py
@@ -3,32 +3,28 @@ import gmplot
 import seaborn as sns
 import sys
 
-def plot(places, name, api):
+def plot(places, name, groupNum, api):
     #plot data points on map
     colors = ["#000000", "#000080", "#00008B", "#0000CD", "#0000FF", "#006400", "#008000", "#008080", "#008B8B", "#00BFFF", "#00CED1", "#00FA9A", "#00FF00", "#00FF7F", "#00FFFF", "#191970", "#1E90FF", "#20B2AA", "#228B22", "#2E8B57", "#2F4F4F", "#32CD32", "#3CB371", "#40E0D0", "#4169E1", "#4682B4", "#483D8B", "#48D1CC", "#4B0082", "#556B2F", "#5F9EA0", "#6495ED", "#66CDAA", "#696969", "#6A5ACD", "#6B8E23", "#708090", "#778899", "#7B68EE", "#7CFC00", "#7FFF00", "#7FFFD4", "#800000", "#800080", "#808000", "#808080", "#87CEEB", "#87CEFA", "#8A2BE2", "#8B0000", "#8B008B", "#8B4513", "#8FBC8F", "#90EE90", "#9370DB", "#9400D3", "#98FB98", "#9932CC", "#9ACD32", "#A0522D", "#A52A2A", "#A9A9A9", "#ADD8E6", "#ADFF2F", "#AFEEEE", "#B0C4DE", "#B0E0E6", "#B22222", "#B8860B", "#BA55D3", "#BC8F8F", "#BDB76B", "#C0C0C0", "#C71585", "#CD5C5C", "#CD853F", "#D2691E", "#D2B48C", "#D3D3D3", "#D8BFD8", "#DA70D6", "#DAA520", "#DB7093", "#DC143C", "#DCDCDC", "#DDA0DD", "#DEB887", "#E0FFFF", "#E6E6FA", "#E9967A", "#EE82EE", "#EEE8AA", "#F08080", "#F0E68C", "#F0F8FF", "#F0FFF0", "#F0FFFF", "#F4A460", "#F5DEB3", "#F5F5DC", "#F5F5F5", "#F5FFFA", "#F8F8FF", "#FA8072", "#FAEBD7", "#FAF0E6", "#FAFAD2", "#FDF5E6", "#FF0000", "#FF00FF", "#FF1493", "#FF4500", "#FF6347", "#FF69B4", "#FF7F50", "#FF8C00", "#FFA07A", "#FFA500", "#FFB6C1", "#FFC0CB", "#FFD700", "#FFDAB9", "#FFDEAD", "#FFE4B5", "#FFE4C4", "#FFE4E1", "#FFEBCD", "#FFEFD5", "#FFF0F5", "#FFF5EE", "#FFF8DC", "#FFFACD", "#FFFAF0", "#FFFAFA", "#FFFF00", "#FFFFE0", "#FFFFF0", "#FFFFFF"]
     # colors = sns.color_palette(colorOptions, numGroups, as_cmap=True)
     # colors = list(map(lambda x: '#%02x%02x%02x' % (int(x[0]*255), int(x[1]*255), int(x[2]*255)), palette))
     # colors = ['magenta', 'turquoise', 'red', 'blue', 'green', 'yellow', 'purple', 'pink', 'orange', 'brown'] #colors of groups
-    # colors = sns.color_palette(colors, numGroups, as_cmap=True)
+    colors = sns.color_palette(colors, numGroups, as_cmap=True)
     gmap = gmplot.GoogleMapPlotter.from_geocode(name, apikey=api)
-    for i in range(len(places['Color'].unique())):
-        group = places[places.Color == i]
-        gmap.scatter(group.Lat.tolist(), group.Lng.tolist(), color=colors[i % len(colors)])
+    group = places[places.Color == groupNum]
+    gmap.scatter(group.Lat.tolist(), group.Lng.tolist(), color=colors[groupNum % len(colors)])
     gmap.draw(r"maps\\" + name + "_map.html")
 
 if __name__ == '__main__':
     api = ''
     name = ''
     numGroups = 0
-    if len(sys.argv) != 3:
-        print('Usage: plot.py <GOOGLE API KEY> <CITY NAME>')
-        exit(1)
-    else:
-        try:
-            api = sys.argv[1]
-            name = sys.argv[2]
-        except:
-            print('Usage: plot.py <GOOGLE API KEY> <CITY NAME>')
+    try:
+        api = sys.argv[1]
+        name = sys.argv[2]
+        groupNum = int(sys.argv[3])
+    except:
+        print('Usage: plot.py <GOOGLE API KEY> <CITY NAME> <GROUP NUM>')
     places = pd.read_csv(r"files\\" + name + "_results.csv")
-    plot(places, name, api)
+    plot(places, name, groupNum, api)
 
